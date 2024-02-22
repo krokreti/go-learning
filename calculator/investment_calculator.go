7f0f197d0d56e8ba4f08f93774bc9ff581434bd4
Davi Pinheiro <davipinheiro1993@gmail.com>
2024-02-21 23:18:07 -0300

    multi return function

--- calculator/investment_calculator.go
+++ calculator/investment_calculator.go
@@ -5,12 +5,14 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	expectedReturnRate := 5.5
 	var investmentAmount, years float64 = 1000 , 10
 
-	fmt.Println("Please enter a investment value: ")
+	// fmt.Println("Please enter a investment value: ")
+	outputText("Please enter a investment value:")
 	fmt.Scan(&investmentAmount)
 
 	fmt.Println("How long should you be waiting for? ")
@@ -19,10 +21,19 @@ func main() {
 	fmt.Println("Whats your expected rate? ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years);
 
 	//fmt.Println("Future value: ", futureValue);
 	// fmt.Println("Future value (inflation): ", futureRealValue)
 	fmt.Printf("Future Value: %.2f\nFuture value (inflation): %.2f\n", futureValue, futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	futureValue :=investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	realFutureValue := futureValue / math.Pow(1+inflationRate/100, years)
+	return futureValue, realFutureValue
 }
